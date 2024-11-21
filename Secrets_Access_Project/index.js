2531e8c0f850fb632c802a2f5521c910841c0aaf
Sibi-Sambasivam <151834749+Sibi-Sambasivam@users.noreply.github.com>
2024-11-21 10:48:51 +0530

    Add files via upload

--- Secrets_Access_Project/index.js
+++ Secrets_Access_Project/index.js
@@ -1,41 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express();
 const port = 3000;
+const app = express();
 
 var userIsAuthorised = false;
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-function passwordCheck(req, res, next) {
-    const password = req.body["password"];
-    if (password === "ILoveProgramming") {
-      userIsAuthorised = true;
-    }
-    next();
-  }
-  app.use(passwordCheck);
+function passwordchecker(req,res,next) {
+  var password = req.body['password'];
+  if (password === "ILoveProgramming") {
+  userIsAuthorised = true;
+  }next();
   
-  app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
-  
-  app.post("/check", (req, res) => {
+}
+
+app.use(passwordchecker);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+app.post("/check", (req, res) => {
     if (userIsAuthorised) {
-      res.sendFile(__dirname + "/public/secret.html");
+  res.sendFile(__dirname+ "/public/secret.html");
     } else {
-      res.sendFile(__dirname + "/public/index.html");
-     
+  res.sendFile(__dirname + "/public/index/html");
     }
-  });
-  
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-  
+});
 
 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
